e7b3afa88896be1a267cf882ee5d21091a0b94ea
root <root@MonaDevice01.(none)>
2017-09-11 23:29:53 +0000

    debugged pi_pins on the raspberry pi. Querying the output and input pins now works

--- pi_pins.py
+++ pi_pins.py
@@ -3,18 +3,20 @@ import RPi.GPIO as GPIO
 
 
 class pi1_pins(object):
-    __ms_out = 23  # GPIO Pin 23
-    __ms_in = 24  # GPIO Pin 24
+    __ms_out = 16  # GPIO Pin 23
+    __ms_in = 18  # GPIO Pin 24
     
     def __init(self):
         #initialization
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(__ms_out, GPIO.OUT)  # Initialize GPI023 as digital output
-        GPIO.setup(__ms_in, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        # Initialize GPI023 as digital input
-        GPIO.output(__ms_out, GPIO.HIGH)  # Set to HIGH
 
     def mona_switch_ping(self):
         #Query if the face-switch connection on the painting is turned to ON
-        return GPIO.input(__ms_in)
+	GPIO.setmode(GPIO.BOARD)
+	#GPIO.setup(self.__ms_in, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.__ms_out, GPIO.OUT)
+	GPIO.output(self.__ms_out, GPIO.HIGH)
+	GPIO.setup(self.__ms_in, GPIO.IN)
+	return GPIO.input(18)
+	#return GPIO.input(self.__ms_in)
 
